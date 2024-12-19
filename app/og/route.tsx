2c26c40b0686def27e304e2447f9c20c2611d57f
Jack Sellwood <jacksellwood@me.com>
2024-12-19 09:55:42 -0600

    tweak OG image

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -7,8 +7,8 @@ export function GET(request: Request) {
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+        <div tw="flex flex-col md:flex-row w-full py-12 px-4 pl-12 md:items-center justify-between p-8">
+          <h2 tw="flex flex-col text-9xl font-bold tracking-tight text-left">
             {title}
           </h2>
         </div>
